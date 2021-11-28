513dd18ac68faa84540029f2c93303361c5db279
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-28 16:27:31 +0900

    userモデル修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nickname
+    # validates :gender
+    validates :birthday
+  end
+
+  has_many :managements
+  has_many :posts
+  has_many :comments
 end
