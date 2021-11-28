6dca7bf3c437baed20fcc7d87e91c017a5bee1af
Tomoya Kine <kineeeecchi@gmail.com>
2021-11-28 16:25:03 +0900

    ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "managements#index"
+  resources :ingredients, only: [:index, :new, :create]
+  resources :managements, only: [:index, :new, :create]
+  resources :posts,       only: [:index, :new, :create]
 end
